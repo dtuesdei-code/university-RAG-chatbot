c583f9cac89c4d3ec7d6aea425b2f50e5461138f
dtuesdei code <david2sday112@gmail.com>
2026-06-11 21:32:31 +0300

    fix and update app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain_chroma import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from difflib import SequenceMatcher
 import re
